ea4182fa104bce3e29cc1b3173aab272d6b2c486
Odonno <bottiau.david@laposte.net>
2019-02-01 17:01:46 +0100

    feat: add all export in index file

--- index.ts
+++ index.ts
@@ -1,5 +1,38 @@
-export { SignalRError, createHub, findHub } from "./src/hub";
+export { 
+    SIGNALR_CONNECTED,
+    SIGNALR_CONNECTING,
+    SIGNALR_CREATE_HUB,
+    SIGNALR_DISCONNECTED,
+    SIGNALR_ERROR,
+    SIGNALR_HUB_FAILED_TO_START,
+    SIGNALR_HUB_STARTED,
+    SIGNALR_HUB_UNSTARTED,
+    SIGNALR_RECONNECTING,
+    SIGNALR_START_HUB,
+    SignalRAction,
+    SignalRConnectedAction,
+    SignalRConnectingAction,
+    SignalRCreateHubAction,
+    SignalRDisconnectedAction,
+    SignalRErrorAction,
+    SignalRHubFailedToStartAction,
+    SignalRHubStartedAction,
+    SignalRHubUnstartedAction,
+    SignalRReconnectingAction,
+    SignalRStartHubAction,
+    createSignalRHub,
+    startSignalRHub 
+} from './src/actions';
+export { SignalREffects } from './src/effects';
+export { SignalRError, SignalRHub, createHub, findHub } from "./src/hub";
 export { SignalRStates, SignalRHubState, SignalRHubStatus } from './src/hubStatus';
+export {
+    StateKeyOrSelector,
+    StoreSignalRConnectionModule,
+    StoreSignalRConfig,
+    SIGNALR_CONFIG
+} from './src/module';
 export { BaseSignalRStoreState, signalrReducer } from './src/reducer';
+
 
 // TODO : provide a way to get the current state of a SignalRHub (findHub selector?)
