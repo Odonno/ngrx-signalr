f04d85792e448d517468217096459843361071d4
Odonno <bottiau.david@laposte.net>
2019-10-05 12:01:11 +0200

    chore: remove compiled files from source control

--- dist/src/storeSignalrService.d.ts
+++
@@ -1,5 +0,0 @@
-import { HubKeyDefinition } from "./models";
-export declare class StoreSignalRService {
-    findHub(x: string | HubKeyDefinition, url?: string | undefined): import("./hub").ISignalRHub | undefined;
-    createHub(hubName: string, url?: string | undefined): import("./hub").ISignalRHub | undefined;
-}
